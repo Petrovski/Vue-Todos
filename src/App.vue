4dcd59d9e0a249763732405bc321a11adc546337
FORTRESS <alexpetroski@gmail.com>
2019-07-23 11:20:47 -0400

    Pushing Vue.js Todo App to Github

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+	<div id="app">
+		<Header />
+		<AddTodo v-on:add-todo="addTodo" />
+		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+	</div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+import axios from "axios";
+
+export default {
+	name: "app",
+	components: {
+		Header,
+		AddTodo,
+		Todos
+	},
+	methods: {
+		deleteTodo(id) {
+			this.todos = this.todos.filter(todo => todo.id !== id);
+		},
+		addTodo(newTodo) {
+			this.todos = [...this.todos, newTodo];
+		}
+	},
+	data() {
+		return {
+			todos: []
+		};
+	},
+	created() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+			.then(res => (this.todos = res.data))
+			.catch(err => console.log(err));
+	}
+};
+</script>
+
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	line-height: 1.4;
+}
+
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
+.btn:hover {
+	background: #666;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+	<header class="header">
+		<h1>Todo List</h1>
+		<img alt="Vue logo" src="../../assets/logo.png" />
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+	background: #333;
+	color: #fff;
+	text-align: center;
+	padding: 10px;
+}
+.header a {
+	color: #fff;
+	padding-right: 5px;
+	text-decoration: none;
+}
+
+img {
+	height: 50px;
+	width: 50px;
+	margin-top: 20px;
+}
+</style>
