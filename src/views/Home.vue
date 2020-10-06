6e9a409a55e2fb16d0578640d22633a1839c166e
Alex Petroski <alexpetroski@gmail.com>
2020-10-06 07:56:39 -0400

    Updating packages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-	<div id="app">
-		<AddTodo v-on:add-todo="addTodo" />
-		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-	</div>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
@@ -11,72 +11,74 @@ import Todos from "../components/Todos";
 import axios from "axios";
 
 export default {
-	name: "Home",
-	components: {
-		AddTodo,
-		Todos
-	},
-	methods: {
-		deleteTodo(id) {
-			axios
-				.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-				.then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
-				.catch(err => console.log(err));
-		},
-		addTodo(newTodo) {
-			const { title, completed } = newTodo;
+  name: "Home",
+  components: {
+    AddTodo,
+    Todos,
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(
+          (res) => (this.todos = this.todos.filter((todo) => todo.id !== id))
+        )
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
 
-			axios
-				.post("https://jsonplaceholder.typicode.com/todos", {
-					title,
-					completed
-				})
-				.then(res => (this.todos = [...this.todos, res.data]))
-				.catch(err => console.log(err));
-		}
-	},
-	data() {
-		return {
-			todos: []
-		};
-	},
-	created() {
-		axios
-			.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-			.then(res => (this.todos = res.data))
-			.catch(err => console.log(err));
-	}
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
 <style>
 * {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 body {
-	line-height: 1.4;
+  line-height: 1.4;
 }
 
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 .btn {
-	display: inline-block;
-	border: none;
-	background: #555;
-	color: #fff;
-	padding: 7px 20px;
-	cursor: pointer;
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
 }
 .btn:hover {
-	background: #666;
+  background: #666;
 }
 </style>
